35e593652fd1e0286d14b1ed6a5fba2e95ad28b0
Michael Markieta <markieta.m@gmail.com>
2020-08-17 15:32:41 -0400

    wasm cannot use udp

--- client/main.go
+++ client/main.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"log"
-	"net"
-)
-
 func main() {
-	log.Printf("Connecting to game server...")
-	addr, err := net.ResolveUDPAddr("udp4", "35.203.44.130:7777")
-	conn, err := net.DialUDP("udp", nil, addr)
-	if err != nil {
-		log.Fatalf("Error connecting to game server: %s", err)
-	}
-	log.Printf("... connected")
-	_, err2 := conn.Write([]byte("hello\n"))
-	if err2 != nil {
-		log.Fatalf("Error connecting to game server: %s", err)
-	}
-	buffer := make([]byte, 1024)
-	n, _, err := conn.ReadFromUDP(buffer)
-	log.Printf("%s", string(buffer[0:n]))
-	defer conn.Close()
-	log.Printf("Connection closed.")
+	// THIS WORKS, BUT NOT IN WASM, BECAUSE UDP IS NOT SUPPORTED BY BROWSERS
+	//log.Printf("Connecting to game server...")
+	//addr, err := net.ResolveUDPAddr("udp4", "35.203.44.130:7777")
+	//conn, err := net.DialUDP("udp", nil, addr)
+	//if err != nil {
+	//	log.Fatalf("Error connecting to game server: %s", err)
+	//}
+	//log.Printf("... connected")
+	//_, err2 := conn.Write([]byte("hello\n"))
+	//if err2 != nil {
+	//	log.Fatalf("Error connecting to game server: %s", err)
+	//}
+	//buffer := make([]byte, 1024)
+	//n, _, err := conn.ReadFromUDP(buffer)
+	//log.Printf("%s", string(buffer[0:n]))
+	//defer conn.Close()
+	//log.Printf("Connection closed.")
 }
